4d486056e1cb7a1d86c31083cc9fb47ee665bb62
alpha <alpha5611331@gmail.com>
2024-12-09 05:00:53 -0500

    commented

--- parse_wayfair.py
+++ parse_wayfair.py
@@ -8,10 +8,10 @@ from loguru import logger
 
 CUR_DIR = Path(__file__).parent
 
-html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-51-54.html"
-html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-53-05.html"
-html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-53-17.html"
 html_path = CUR_DIR / "wayfair-variation.html"
+html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-53-17.html"
+html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-53-05.html"
+html_path = CUR_DIR / "wayfair_detail_2024-12-08_12-51-54.html"
 output_path = CUR_DIR / "wayfair-result.json"
 
 
@@ -40,20 +40,30 @@ def parse(html_path: Path, output_path: Path):
         product_data = get_from_json(data_json, ["application", "props", "productData"])
         price_data = get_from_json(product_data, ["price"])
 
+        # ================================
         # Product url
+        # ================================
         result["product_url"] = page_elem.select_one("link[rel=canonical]").attrs["href"]
 
+        # ================================
         # Result count
+        # ================================
         result["result_count"] = 1
 
+        # ================================
         # Name
+        # ================================
         detail: dict[str, Any] = {}
         detail["name"] = content_elem.select_one("a.HotDealsProductTitle").text.strip()
 
+        # ================================
         # Main Image
+        # ================================
         detail["main_image"] = content_elem.select_one("div.ProductDetailSingleMediaViewer").select_one("img").attrs["src"]
 
+        # ================================
         # Images
+        # ================================
         list_elem = content_elem.select_one("ul.HotDealsThumbnailCarousel-container")
         item_elem_list = list_elem.select("li")
         images: list[str] = []
@@ -65,7 +75,9 @@ def parse(html_path: Path, output_path: Path):
             images.append(image_link)
         detail["images"] = images
 
+        # ================================
         # Price
+        # ================================
         price = get_from_json(price_data, ["customerPrice", "quantityPrice", "value"])
         if price is None:
             price_elem = content_elem.select_one("div.BasePriceBlock")
@@ -79,7 +91,9 @@ def parse(html_path: Path, output_path: Path):
                     raise ValueError(f"Invalid input: {price_str}")
         detail["price"] = price
 
+        # ================================
         # List Price
+        # ================================
         list_price = None
         price_elem = content_elem.select_one("div.BasePriceBlock--list")
         if price_elem is not None:
@@ -93,64 +107,98 @@ def parse(html_path: Path, output_path: Path):
             list_price = get_from_json(price_data, ["listPrice", "quantityPrice", "value"])
         detail["list_price"] = list_price
 
+        # ================================
         # Currency
+        # ================================
         currency = get_from_json(price_data, ["customerPrice", "quantityPrice", "currency"])
         if currency is None:
             price_str = content_elem.select_one("div.BasePriceBlock").text
             currency = price_str[:1]
         detail["currency"] = currency
 
+        # ================================
         # Description
+        # ================================
         detail["description"] = None
 
+        # ================================
         # SKU ID
+        # ================================
         detail["sku_id"] = content_elem.select_one("form.HotDealsCallToActionForm").select_one("input[name=sku]").attrs["value"]
 
+        # ================================
         # Brand
+        # ================================
         brand_str = content_elem.select_one("p.HotDealsProductTitle-manufacturerName").text
         detail["brand"] = brand_str.replace("By", "").strip()
 
+        # ================================
         # Rating
+        # ================================
         rating_str_list = content_elem.select_one("button[data-hb-id=ReviewStars]>p").contents
         detail["rating"] = float(rating_str_list[0].split()[1].strip())
 
+        # ================================
         # Total ratings
+        # ================================
         detail["total_ratings"] = int(rating_str_list[2].split()[0].strip())
 
+        # ================================
         # Total reviews
+        # ================================
         detail["total_reviews"] = None
 
+        # ================================
         # Reviews
+        # ================================
         detail["reviews"] = []
 
+        # ================================
         # Retailer Badge
+        # ================================
         detail["retailer_badge"] = None
 
-        ## Variant
+        # ================================
+        # Variant
+        # ================================
         detail["variant"] = {}
 
-        ## Variants
+        # ================================
+        # Variants
+        # ================================
         detail["variants"] = []
 
+        # ================================
         # Product overview
+        # ================================
         detail["product_overview"] = get_from_json(product_data, ["overview"])
 
+        # ================================
         # Delivery Zipcode
+        # ================================
         detail["delivery_postal_code"] = get_from_json(product_data, ["delivery", "postal_code"])
         detail["delivery_postal_code_city"] = get_from_json(product_data, ["delivery", "postal_code_city"])
 
+        # ================================
         # Shipping Info
+        # ================================
         detail["shipping_info"] = None
         
+        # ================================
         # Features
+        # ================================
         detail["features"] = None
 
-        ## at-a-glance
+        # ================================
+        # at-a-glance
+        # ================================
         detail["at-a-glance"] = None
 
         result["detail"] = detail
 
+        # ================================
         # Remaining credits
+        # ================================
         result["remaining_credits"] = None
 
     with output_path.open("w", encoding="utf-8") as file:
